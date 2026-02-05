fc92d3175fd17378eba750b16af45b2020c8f265
Daris Bouaziz <mkdarice@MacBookAir-de-Daris.local>
2026-02-05 16:10:02 -0500

    Persist Timer Across Re-Renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,48 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 const App = () => {
-  const inputRef = useRef(null);
+  const timerRef = useRef(null);
 
-  const submit = () => {
-    console.log(inputRef.current);
-    console.log(inputRef.current.value);
-    inputRef.current.style.backgroundColor = "red";
-    inputRef.current.style.color = "white";
-    inputRef.current.setAttribute("placeholder", "Updated...");
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const toggleTimer = () => {
+    if (isRunning) {
+      //Clear the interval to stop the timer
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    } else {
+      //Start timer (timerRef = Interval id)
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    setIsRunning(!isRunning);
+  };
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    setIsRunning(false);
+    setTime(0);
+    timerRef.current = null;
   };
 
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
-      <h2 className="text-2xl font-bold mb-4">useRef Example</h2>
-
-      <input
-        type="text"
-        placeholder="Type something..."
-        className="w-full p-2 border rounded-lg"
-        ref={inputRef}
-      />
+      <h2 className="text-4xl font-semibold mt-4">⏳ Timer: {time}</h2>
 
       <button
-        onClick={submit}
-        className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+        onClick={toggleTimer}
+        className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover: bg-green-600 mr-3"
       >
-        Submit
+        {isRunning ? "Pause" : "Start"}
+      </button>
+      <button
+        onClick={resetTimer}
+        className="mt-3 bg-red-500 text-white px-4 py-2 rounded hover: bg-red-600"
+      >
+        Reset
       </button>
     </div>
   );
